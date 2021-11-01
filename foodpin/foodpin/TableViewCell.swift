5434c3adb4332e303fd47e1c686ae18d2de60397
michael21910 <410821305@gms.ndhu.edu.tw>
2021-11-01 16:45:52 +0800

    first version complete, finished at iOS class

---
+++ foodpin/foodpin/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  foodpin
+//
+//  Created by Tsuen Hsueh on 2021/11/1.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- foodpin/foodpin/TableViewController.swift
+++ foodpin/foodpin/TableViewController.swift
@@ -17,6 +17,10 @@ class TableViewController: UITableViewController {
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
     
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
     lazy var dataSource = configureDataSource()
     
     override func viewDidLoad() {
@@ -40,10 +44,12 @@ class TableViewController: UITableViewController {
         let dataSource = UITableViewDiffableDataSource<Section, String>(
             tableView: tableView,
             cellProvider: {  tableView, indexPath, restaurantName in
-                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+                let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! TableViewCell
                 
-                cell.textLabel?.text = restaurantName
-                cell.imageView?.image = UIImage(named: self.restaurantImages[indexPath.row])
+                cell.nameLabel?.text = restaurantName
+                cell.thumbnailImageView?.image = UIImage(named: self.restaurantImages[indexPath.row])
+                cell.locationLabel?.text = self.restaurantLocations[indexPath.row]
+                cell.typeLabel?.text = self.restaurantTypes[indexPath.row]
                 return cell
             }
         )
